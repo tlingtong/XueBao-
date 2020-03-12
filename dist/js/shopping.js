27459962ebf44d6cb8b04993732d9712b9010958
tao <45@qq.com>
2020-03-12 18:49:32 +0800

    jiben finsh

--- dist/js/shopping.js
+++ dist/js/shopping.js
@@ -2,24 +2,29 @@
 
 //   查询购物车中的信息，获取数据
 $.get("http://jx.xuzhixiang.top/ap/api/cart-list.php", {
-  id: "32951"
+  id: "32951" //参数为id
+
 }).then(function (data) {
-  console.log(data); // console.log(data.data[0].pimg);     
+  console.log(data); //可以获取加入购物车的信息
+  // console.log(data.data[0].pimg);     
 
-  var str1 = "";
-  var str2 = "";
+  var str1 = ""; //定义空的字符串用来存放添加数据的标签
 
-  for (var i in data.data) //data.data[i].
+  for (var i in data.data) //data.data[i].   循环添加
   {
-    str1 += "\n            <tr>\n                <td id=\"td1\">\n                 <img src=\"".concat(data.data[i].pimg, "\" alt=\"\">\n                 <p>").concat(data.data[i].pdesc, "</p>\n                </td>\n                <td>\uFFE5").concat(data.data[i].pprice, "</td>\n                <td>").concat(data.data[i].pnum, "</td>\n                <td>\uFFE5").concat(data.data[i].pnum * data.data[i].pprice, "</td>\n                <td>\uFFE5").concat(data.data[i].pnum * data.data[i].pprice, "</td>\n            </tr>\n            ");
-  }
+    str1 += "\n            <tr>\n                <td id=\"td1\">\n                 <img src=\"".concat(data.data[i].pimg, "\" alt=\"\">\n                 <p>").concat(data.data[i].pdesc, "</p>\n                </td>\n                <td>\uFFE5").concat(data.data[i].pprice, "</td>\n                <td>").concat(data.data[i].pnum, "</td>\n                <td>\uFFE5").concat(data.data[i].pnum * data.data[i].pprice, "</td>\n                <td>\uFFE5<span class=\"danPrice\">").concat(data.data[i].pnum * data.data[i].pprice, "</span></td>\n            </tr>\n            ");
+  } // 将str渲染进table里
 
-  $("table").append(str1); // for(let i in data.data)             //data.data[i].
-  // {
-  //     str2 += `
-  //         <div id="num1">商品合计：￥${data.data[i].pnum * data.data[i].pprice}</div>
-  //         <p id="num2">应付总额￥${data.data[i].pnum * data.data[i].pprice}</p>
-  //     `
-  // } 
-  // $(".shopp").append(str2);
+
+  $("table").append(str1); // 计算购物车总价
+
+  var count = 0; //定义空的变量count，用来存放总价的值    
+
+  $(".danPrice").each(function () {
+    //循环遍历表格中存放单个总价的标签
+    // console.log(parseInt($(this).text()));       //264  0  输出添加购物车商品的单个总价
+    count += parseInt($(this).text()); //转为数字 +=给count赋值
+  }); // console.log(count);                               //264  计算结果无误  
+
+  $(".totalPrice").text(count); //将count值赋给页面中的总价
 });
--- js/shopping.js
+++ js/shopping.js
@@ -2,13 +2,12 @@
 
 //   查询购物车中的信息，获取数据
   $.get("http://jx.xuzhixiang.top/ap/api/cart-list.php",{
-        id:"32951"                     
+        id:"32951"                          //参数为id
     }).then(data=>{
-        console.log(data);
+        console.log(data);                 //可以获取加入购物车的信息
         // console.log(data.data[0].pimg);     
-        var str1 = "";
-        var str2 = "";
-        for(let i in data.data)             //data.data[i].
+        var str1 = "";                      //定义空的字符串用来存放添加数据的标签
+        for(let i in data.data)             //data.data[i].   循环添加
         {
             str1 += `
             <tr>
@@ -19,23 +18,24 @@
                 <td>￥${data.data[i].pprice}</td>
                 <td>${data.data[i].pnum}</td>
                 <td>￥${data.data[i].pnum * data.data[i].pprice}</td>
-                <td>￥${data.data[i].pnum * data.data[i].pprice}</td>
+                <td>￥<span class="danPrice">${data.data[i].pnum * data.data[i].pprice}</span></td>
             </tr>
             `
         } 
+        // 将str渲染进table里
         $("table").append(str1);
 
-        
-        // for(let i in data.data)             //data.data[i].
-        // {
-        //     str2 += `
-        //         <div id="num1">商品合计：￥${data.data[i].pnum * data.data[i].pprice}</div>
-        //         <p id="num2">应付总额￥${data.data[i].pnum * data.data[i].pprice}</p>
-        //     `
-        // } 
-       
-        // $(".shopp").append(str2);
 
+        // 计算购物车总价
+        var count =  0;                                      //定义空的变量count，用来存放总价的值    
+        $(".danPrice").each(function(){                     //循环遍历表格中存放单个总价的标签
+            // console.log(parseInt($(this).text()));       //264  0  输出添加购物车商品的单个总价
+             count += parseInt($(this).text());             //转为数字 +=给count赋值
+        })
+        // console.log(count);                               //264  计算结果无误  
+
+        $(".totalPrice").text(count);                       //将count值赋给页面中的总价
+        
 
     })
 
